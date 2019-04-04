6864100bf81d01a7bd200fb6827c72c15c7d9d8f
Laerte <laertefbk@gmail.com>
2019-04-04 19:38:37 -0300

    refactoring flat example

--- Chapter 1 - Principles and Philosophy/flat-example.py
+++ Chapter 1 - Principles and Philosophy/flat-example.py
@@ -3,11 +3,14 @@ y = 399
 
 
 def checker(x, y):
-    if x > 0:
-        if y > 100:
-            raise ValueError("Value for y is too large.")
-        else:
-            return y
+    if x > 0 and y > 100:
+        raise ValueError("Value for y is too large.")
+    elif x > 0:
+        return y
+    elif x == 0:
+        return False
+    else:
+        raise ValueError("Value for x cannot be negative.")
 
 
 print(checker(x, y))
