59f3d06468e618158f44f5ac9f1d7c771316ed19
Laerte <laertefbk@gmail.com>
2019-04-05 15:45:40 -0300

    Catching Exception Example 1

---
+++ Chapter 2 - Advanced Basics/catching-exceptions.py
@@ -0,0 +1,16 @@
+def count_lines(filename):
+    """
+    Count the number of lines in a file. If the file can't be
+    opened, it should be treated the same as if it was empty.
+    """
+    try:
+        return len(open(filename, 'r').readlines())
+    except:
+        print('exception error reading the file or calculating lines!')
+        # Something went wrong reading the file
+        # or calculating the number of lines.
+        return 0
+
+
+myfile = input('Enter a file to open: ')
+print(count_lines(myfile))
