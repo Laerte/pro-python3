d5c6ecaccb0c96a374ca6ce4b86c7b1bfc5d111e
Laerte <laertefbk@gmail.com>
2019-04-05 18:53:16 -0300

    Proceeding Regardless of Exceptions

--- Chapter 2 - Advanced Basics/catching-exceptions.py
+++ Chapter 2 - Advanced Basics/catching-exceptions.py
@@ -6,8 +6,12 @@ def count_lines(filename):
     Count the number of lines in a file. If the file can't be
     opened, it should be treated the same as if it was empty.
     """
+
+    file = None  # file must always have a value
+
     try:
         file = open(filename, 'r')
+        lines = file.readlines()
     except TypeError as e:
         # The filename wasn't valid for use with the filesystem.
         logging.error(e)
@@ -15,8 +19,15 @@ def count_lines(filename):
         # Something went wrong reading the file
         logging.error(e.args[1])
         return 0
+    except UnicodeDecodeError as e:
+        # The contents of the file were in an unknown encoding.
+        logging.error(e)
+        return 0
     else:
-        return len(file.readlines())
+        return len(lines)
+    finally:
+        if file:
+            file.close()
 
 
 myfile = input('Enter a file to open: ')
