7e2b2559f6c92217f099777079c176bf1676d814
Laerte <laertefbk@gmail.com>
2019-04-05 19:06:56 -0300

    The with Statement

--- Chapter 2 - Advanced Basics/catching-exceptions.py
+++ Chapter 2 - Advanced Basics/catching-exceptions.py
@@ -1,33 +1,10 @@
-import logging
-
-
 def count_lines(filename):
     """
-    Count the number of lines in a file. If the file can't be
-    opened, it should be treated the same as if it was empty.
+    Count the number of lines in a file.
     """
 
-    file = None  # file must always have a value
-
-    try:
-        file = open(filename, 'r')
-        lines = file.readlines()
-    except TypeError as e:
-        # The filename wasn't valid for use with the filesystem.
-        logging.error(e)
-    except EnvironmentError as e:
-        # Something went wrong reading the file
-        logging.error(e.args[1])
-        return 0
-    except UnicodeDecodeError as e:
-        # The contents of the file were in an unknown encoding.
-        logging.error(e)
-        return 0
-    else:
-        return len(lines)
-    finally:
-        if file:
-            file.close()
+    with open(filename, 'r') as file:
+        return len(file.readlines())
 
 
 myfile = input('Enter a file to open: ')
