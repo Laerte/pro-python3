95f4aa46e1160dbc77a57c2ffb8c60db8df63c32
Laerte <laertefbk@gmail.com>
2019-04-08 13:15:17 -0300

    Remove ]

--- Chapter 2 - Advanced Basics/fallback-imports.py
+++ Chapter 2 - Advanced Basics/fallback-imports.py
@@ -1,5 +1,5 @@
 try:
-    # Use the new library if available. Added in Python 2.5]
+    # Use the new library if available. Added in Python 2.5
     from hashlib import md5
 except ImportError:
     from md5 import new as md5  # Compatible functionality provided prior to Python 2.5
