98265f55fed9fed6905e704c66800fad29bc2b90
Laerte <laertefbk@gmail.com>
2019-04-09 16:37:37 -0300

    Validating Arguments

--- Chapter 3 - Functions/introspection.py
+++ Chapter 3 - Functions/introspection.py
@@ -26,3 +26,28 @@ def get_arguments(func, args, kwargs):
 
 
 print(get_arguments(example, (1,), {'f': 4}))
+
+
+def validate_arguments(func, args, kwargs):
+    """
+    Given a function and its arguments, return a dictionary
+    with any errors that are posed by the given arguments.
+    """
+    arguments = get_arguments(func, args, kwargs)
+    spec = inspect.getfullargspec(func)
+    declared_args = spec.args[:]
+    declared_args.extend(spec.kwonlyargs)
+    errors = {}
+
+    for name in declared_args:
+        if name not in arguments:
+            errors[name] = "Required argument not provided."
+
+    for name in arguments:
+        if name not in declared_args:
+            errors[name] = "Unknown argument provided."
+
+    return errors
+
+
+print(validate_arguments(example, (1,), {'f': 4}))
