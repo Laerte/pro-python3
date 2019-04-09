71982aec684de2be982f5af0140edb8998c654c2
Laerte <laertefbk@gmail.com>
2019-04-09 16:13:55 -0300

    preloading arguments

---
+++ Chapter 3 - Functions/passing-arguments.py
@@ -0,0 +1,16 @@
+import os
+import functools
+
+
+def load_file(file, base_path='/', mode='rb'):
+    return open(os.path.join(base_path, file), mode)
+
+
+# partial application of the function "preloading arguments"
+# return a new callable that can be used later
+load_writable = functools.partial(load_file, mode='w',
+                                  base_path='/Users/laerte/Projetos/pro-python3/Chapter 3 - Functions')
+
+f = load_writable('example.txt')
+print(f.mode)
+f.close()
