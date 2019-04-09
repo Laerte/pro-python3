409e70c3dcbc568103816a23fc91c788b50fb2e8
Laerte <laertefbk@gmail.com>
2019-04-09 16:15:23 -0300

    Ident

--- Chapter 3 - Functions/passing-arguments.py
+++ Chapter 3 - Functions/passing-arguments.py
@@ -8,7 +8,8 @@ def load_file(file, base_path='/', mode='rb'):
 
 # partial application of the function "preloading arguments"
 # return a new callable that can be used later
-load_writable = functools.partial(load_file, mode='w',
+load_writable = functools.partial(load_file,
+                                  mode='w',
                                   base_path='/Users/laerte/Projetos/pro-python3/Chapter 3 - Functions')
 
 f = load_writable('example.txt')
