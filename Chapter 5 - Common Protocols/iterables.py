a8fc242b72d696db140d9a892e095aab360f75ca
Laerte <laertefbk@gmail.com>
2020-10-27 23:51:09 -0300

    rewrite Range method

--- Chapter 5 - Common Protocols/iterables.py
+++ Chapter 5 - Common Protocols/iterables.py
@@ -1,23 +1,10 @@
 class Range:
     def __init__(self, count):
         self.count = count
-    def __iter__(self):
-        return RangeIter(self.count)
-
-
-class RangeIter:
-    def __init__(self, count):
-        self.count = count
-        self.current = 0
-    def __iter__(self):
-        return self
-
-    def __next__(self):
-        value = self.current
-        self.current += 1
-        if self.current > self.count:
-             raise StopIteration
-        return value
+    def __getitem__(self, index):
+        if index < self.count:
+            return index
+        raise IndexError
 
 def range_gen(count):
     for x in range(count):
