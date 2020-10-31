cb77180325ce937e88e62dadb94862d401f49176
Laerte <laertefbk@gmail.com>
2020-10-31 15:03:47 -0300

    Borg Pattern

---
+++ Chapter 6 - Object Management/borg_pattern.py
@@ -0,0 +1,17 @@
+class Borg:
+    _namespace = {}
+    def __init__(self):
+        self.__dict__ = Borg._namespace
+        # Do more interesting stuff here.
+
+a = Borg()
+b = Borg()
+
+print(hasattr(a, 'attribute'))
+
+b.attribute = 'value'
+
+print(hasattr(a, 'attribute'))
+print(a.attribute)
+
+print(Borg._namespace)
