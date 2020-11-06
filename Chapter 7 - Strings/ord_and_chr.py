fd69ab618e853630497fb8676ec588d963501ba9
Laerte <laertefbk@gmail.com>
2020-11-06 08:14:37 -0300

    chr() and ord() example

---
+++ Chapter 7 - Strings/ord_and_chr.py
@@ -0,0 +1,8 @@
+print(ord(b'A'))
+print(ord(b'!'))
+print(list(b'Example'))
+
+print(chr(65))
+print(chr(33))
+print([chr(o) for o in [69, 120, 97, 109, 112, 108, 101]])
+print("".join(chr(o) for o in [69, 120, 97, 109, 112, 108, 101]))
